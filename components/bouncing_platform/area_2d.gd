77bac27e9762be06b172bc3c31558ef5d93e2154
Will Thompson <wjt@endlessaccess.org>
2025-09-16 12:36:48 +0100

    bouncing_platform: Fix coding style nit
    Our coding style checker enforces two spaces before the `#` on an inline
    comment like this.

--- components/bouncing_platform/area_2d.gd
+++ components/bouncing_platform/area_2d.gd
@@ -5,6 +5,6 @@ extends Area2D
 
 
 func _on_body_entered(body):
-	if body is CharacterBody2D: # Assuming your player is a CharacterBody2D
+	if body is CharacterBody2D:  # Assuming your player is a CharacterBody2D
 		# Apply an upward impulse to the player
 		body.velocity.y = -bounce_strength
